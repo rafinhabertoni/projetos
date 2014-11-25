a987b3fd328b3fb45f6f3c743e1cd4875ac0b82f
rafinhabertoni <rafael.bertoni87@gmail.com>
2014-11-24 22:16:45 -0200

    Curso MVC

--- Quiron.LojaVirtual/Quiron.LojaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual/Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
@@ -7,8 +8,22 @@ namespace Quiron.LojaVirtual.UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestarPaginacao()
         {
+            /*System.Web.WebPages.Html.HtmlHelper html = null;
+
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensTotal = 28,
+                ItensPorPagina = 10
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            System.Web.Mvc.MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+            Assert.AreEqual("Teste", resultado);*/
         }
     }
 }
